e007690acf91dcde5b9a9a6d3d897bda2c81e0c8
phumoonlight <phumoonlight@hotmail.com>
2022-02-18 16:17:01 +0700

    update router

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
-import { createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from './pages/Index.vue'
+
+const routes = [{ path: '/', component: Index }]
 
 export const router = createRouter({
-  history: createWebHistory(),
-  routes: [],
+	history: createWebHistory(),
+	routes,
 })
